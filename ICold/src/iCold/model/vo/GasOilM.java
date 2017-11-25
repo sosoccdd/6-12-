3b2b71568d392f7af737191cebab519ba2d6c5f9
wlgus <wlgus@192.168.10.111>
2017-11-25 19:18:11 +0900

    김지현 주유소게임 수정

--- ICold/src/iCold/model/vo/GasOilM.java
+++ ICold/src/iCold/model/vo/GasOilM.java
@@ -1,5 +1,100 @@
 package iCold.model.vo;
 
-public class GasOilM {
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/*import gasOilM.GasOilM;
+import gasOilM.GasOilT;*/
+import iCold.view.GasOilV;
+
+public class GasOilM extends GasOilV implements Runnable{
+
+	int won;
+	JFrame sf = new JFrame();
+
+
+	public GasOilM(){
+		
+/*		GasOilM gasm = new GasOilM();
+		Thread t1 = new Thread(new GasOilM());
+		Thread t2 = new Thread(gasm);
+		//Thread t3 = new Thread(new GasThreadTest());
+
+		t2.setDaemon(true);
+		
+		t1.start();
+		t2.start();
+		
+		if(!t2.isAlive()){
+			gasm.stopOilM();
+		}
+		*/
+		
+		
+		
+	}
+
+
+	@Override
+	public void run(){
+		try {
+			for(int i = 0; i<=100; i++){
+				System.out.println(i);
+				Thread.sleep(100);
+				if(i==100){
+					JFrame sf = new JFrame();
+					sf.setTitle("시간 종료");
+					sf.setBounds(300, 300, 500, 500);
+					sf.setLayout(null);
+
+					int sum = 0;
+
+					JLabel slabel = new JLabel("획득 : " + "2222" + " 원");
+					slabel.setSize(100, 100);
+					slabel.setLocation(100, 50);
+					JLabel dlabel = new JLabel("합계 : " + sum + " 원");
+					dlabel.setSize(100, 100);
+					dlabel.setLocation(100, 150);		
+					JButton fbutton = new JButton("돌아가기");
+					fbutton.setSize(70, 70);
+					fbutton.setLocation(100, 350);
+
+					sf.add(slabel);
+					sf.add(dlabel);
+					sf.add(fbutton);
+
+					fbutton.addActionListener(new ActionListener() {			
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							// txt파일에 저장
+							//gasm.stopOilM();
+							sf.setVisible(false);
+
+						}
+					});
+
+					sf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					sf.setVisible(true);
+				}
+			}
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	
+	/*if(!jr.isAlive()){
+		gasm.stopOilM();
+	}*/
+
+
+}
 }
--- ICold/src/iCold/view/GasOilV.java
+++ ICold/src/iCold/view/GasOilV.java
@@ -1,5 +1,218 @@
 package iCold.view;
 
-public class GasOilV {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.plaf.LabelUI;
+
+
+
+public class GasOilV extends JFrame implements Runnable{
+	int count = 0;
+	int coincount = 0;
+	JFrame jr = new JFrame("주유소 게임");
+
+	public GasOilV(){}
+
+	//GasOil 스레드
+	@Override
+	public void run() {
+		this.gasOilPlay();
+		
+		
+	}
+	
+	public void stopOilM(){
+		jr.setVisible(false);
+	}
+	
+	
+	public int getCoincount(){
+		return coincount;
+	}
+	
+	
+	public void gasOilPlay(){
+		
+		jr.setBounds(200, 300, 1000, 800);
+		jr.setLayout(null);
+
+		ImageIcon oilimage = new ImageIcon("images\\oil.png");
+		//ImageIcon back = new ImageIcon("images\\back.jpg");
+		ImageIcon al = new ImageIcon("images\\default.png");
+		ImageIcon son1 = new ImageIcon("images\\son1.png");
+		ImageIcon son2 = new ImageIcon("images\\son2.png");
+		ImageIcon oil_e = new ImageIcon("images\\oil_e.png");
+		ImageIcon money = new ImageIcon("images\\money.png");
+		ImageIcon son1_mo = new ImageIcon("images\\son1_money.png");
+		ImageIcon son2_mo = new ImageIcon("images\\son2_money.png");
+		ImageIcon alget = new ImageIcon("images\\money_get.png");
+		//ImageIcon  = new ImageIcon("images\\money.jpg");
+		ImageIcon  oil[] = new ImageIcon[4];
+		oil[0] = new ImageIcon("images\\oil_1.png");
+		oil[1] = new ImageIcon("images\\oil_2.png");
+		oil[2] = new ImageIcon("images\\oil_3.png");
+		oil[3] = new ImageIcon("images\\oil_f.png");
+
+		ImageIcon coin = new ImageIcon("images\\coin1.png");
+
+		JPanel oilpanel = new JPanel();
+		JLabel oillabel = new JLabel(oilimage);
+		oilpanel.add(oillabel);
+		oilpanel.setSize(150, 150);
+		oilpanel.setLocation(200, 300);
+
+		JPanel alpanel = new JPanel();
+		JLabel allabel = new JLabel(al);
+		alpanel.add(allabel);
+		alpanel.setSize(150, 180);
+		alpanel.setLocation(350, 300);
+
+		JPanel sonpanel = new JPanel();
+		JLabel sonlabel1 = new JLabel(son1);
+		sonpanel.add(sonlabel1);
+		sonpanel.setSize(220, 250);
+		sonpanel.setLocation(550, 300);
+
+		JPanel coinpanel = new JPanel();
+		JLabel coinlabel = new JLabel(coin);
+		coinpanel.add(coinlabel);
+		coinpanel.setSize(200, 200);
+		coinpanel.setLocation(250, 150);
+
+		JPanel gagepanel = new JPanel();
+		JLabel gagelabel = new JLabel(oil_e);
+		gagepanel.add(gagelabel);
+		gagepanel.setSize(70, 100);
+		gagepanel.setLocation(180, 450);
+
+		JPanel totalpanel = new JPanel();
+		JLabel totallabel = new JLabel("획 득 : " + coincount);		
+		totallabel.setSize(100, 50);
+		totallabel.setLocation(100, 50);
+		JLabel totallabel2 = new JLabel("전체 코인 : " + "나중에 합침");	
+		totallabel2.setSize(100, 50);
+		totallabel2.setLocation(200, 50);
+		JButton totalbutton = new JButton();
+		totalbutton.setSize(100,100);
+		totalbutton.setLocation(300, 50);
+
+		totalpanel.setSize(250, 250);
+		totalpanel.setLocation(250, 250);		
+
+		totalpanel.add(totallabel);
+		totalpanel.add(totallabel2);
+		totalpanel.add(totalbutton);
+
+		jr.add(oilpanel);
+		jr.add(alpanel);
+
+		jr.add(sonpanel);
+
+		do{
+			oilpanel.addMouseListener(new MouseAdapter(){
+				@Override
+				public void mouseClicked(MouseEvent e){								
+					jr.add(gagepanel);					
+					jr.revalidate();             //JPanel 리프레시
+					jr.repaint();
+				}
+			});
+
+			jr.addKeyListener(new KeyAdapter() {			
+				@Override
+				public void keyPressed(KeyEvent e){				
+					if(gagepanel.isVisible()){
+
+						while(count <= 4){						
+							if(count==4){
+								sonlabel1.setIcon(son1_mo);
+								break;
+							}
+							if(e.getKeyChar() == ' '){
+								count++;
+								gagelabel.setIcon(oil[count-1]);
+								break;
+							}
+						}
+					}
+				}
+			});
+
+			sonpanel.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e){
+					sonpanel.setSize(220, 250);
+					sonpanel.setLocation(240, 450);
+					if(count != 0){									
+						sonlabel1.setIcon(son1);
+						allabel.setIcon(alget);
+					}					
+				}
+			});
+
+			alpanel.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e){
+					if(allabel.getIcon() == alget){
+						allabel.setIcon(al);
+						jr.add(coinpanel);
+						jr.remove(gagepanel);
+						jr.remove(sonpanel);
+						jr.revalidate();             //JPanel 리프레시
+						jr.repaint();
+
+					}				
+				}
+			});
+
+			coinpanel.addMouseListener(new MouseAdapter(){
+				@Override
+				public void mouseClicked(MouseEvent e){
+					jr.remove(coinpanel);
+					jr.revalidate();             //JPanel 리프레시
+					jr.repaint();
+					count = 0;
+					coincount+=50;
+					System.out.println(coincount);
+					sonpanel.setSize(220, 250);
+					sonpanel.setLocation(550, 300);						
+					gagelabel.setIcon(oil_e);
+					jr.add(sonpanel);	
+
+				}
+			});	
+
+		}while(coincount>300);
+		
+		//jr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jr.setVisible(true);
+		
+		System.out.println("주유소 종료");
+	
+		
+	}
+
+
 
 }
+
+
+
