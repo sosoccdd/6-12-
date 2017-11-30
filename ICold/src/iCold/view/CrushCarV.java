4998c24f82f131bfe85aa818a92decac50f861e5
sosoccdd <32798013+sosoccdd@users.noreply.github.com>
2017-12-01 05:07:02 +0900

    sdddddddddd

--- ICold/src/iCold/view/CrushCarV.java
+++ ICold/src/iCold/view/CrushCarV.java
@@ -1,36 +1,29 @@
 package iCold.view;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 
-import javax.imageio.ImageIO;
+import java.io.File;
+
+
+
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
+
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 import iCold.model.vo.CrushCarM;
 
 
-public class CrushCarV   {
+public class CrushCarV extends JFrame{
+   //IColdFunction function = new IColdFunction();
+   public static JFrame frame = new JFrame("Crush car");
+   public static Clip clip;
    public CrushCarV() {
       
-      JFrame frame = new JFrame("Crush car");
       frame.setContentPane(new CrushCarM());
       frame.pack();
       frame.add(CrushCarM.label);
+      
       
       
       frame.setLayout(null);
@@ -38,12 +31,13 @@ public class CrushCarV   {
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.setVisible(true);
       
+
       
       try
       {
-         AudioInputStream backmusic = AudioSystem.getAudioInputStream(new File("Sound\\sd3-315.wav"));
+         AudioInputStream backmusic = AudioSystem.getAudioInputStream(new File("Sound\\sd3-306.wav"));
 //       AudioInputStream punchSound = AudioSystem.getAudioInputStream(new File("Sound\\Å¸°ÝÀ½_Çª¾ï.wav"));
-         Clip clip = AudioSystem.getClip();
+         clip = AudioSystem.getClip();
          clip.stop();
          clip.open(backmusic);
 //   clip.open(punchSound);
@@ -56,4 +50,5 @@ public class CrushCarV   {
       
       
    }
+
 }
